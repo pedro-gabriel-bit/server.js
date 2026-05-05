ef8df7a79c11b951ca87504add7b3ce971fce02c
pedro-gabriel-bit <pedrogabriel7000@gmail.com>
2026-05-05 16:59:13 -0300

    subindo servider com node

--- ElementosAndMetodos.js
+++ ElementosAndMetodos.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const seraver = http.createServer((req, res) => {
   console.log(req.method); // Imprime o método HTTP da requisição (GET, POST, etc)
-  res.writable(200, { 'Content-Type': 'text/plain' }); // Writable: Define o status da resposta e os headersgit branch -M main
+  res.writeHead(200, { 'Content-Type': 'text/plain' }); // Writable: Define o status da resposta e os headersgit branch -M main
   res.end(`Metodo recebido: ${req.method}`);
 });
 
