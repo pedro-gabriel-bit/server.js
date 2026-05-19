c5b6335fce8fe850e2cab41ac064c73ae70f203c
pedro-gabriel-bit <pedrogabriel7000@gmail.com>
2026-05-19 15:03:06 -0300

    adicionando metodos GET, POST, PUT e DELETE

--- fazendoCRUD_02.js
+++ fazendoCRUD_02.js
@@ -1,109 +1,134 @@
-// Importando o módulo nativo HTTP do Node.js para criar um servidor web
+// Importando o módulo nativo HTTP do Node.js
 const http = require('http');
-
+ 
+// Simulação de um "banco de dados" em memória usando um array de objetos
 let livros = [
-  {
-    id: 1,
-    titulo: "O Senhor dos Anéis",
-    autor: "J.R.R. Tolkien",
-    ano: 1954
-  },
-
-  {
-    id: 2,
-    titulo: "Harry Potter e a Pedra Filosofal",
-    autor: "J.K. Rowling",
-    ano: 1997
-  },
-
-  {
-    id: 3,
-    titulo: "O pequeno príncipe",
-    autor: "Antoine de Saint-Exupéry",
-    ano: 1943
-  }
+    {
+        id: 1,
+        titulo: 'O pequeno Principe',
+        autor: 'Antonie de Saint-Exupéry'
+    }
 ];
+ 
+// Criação do Servidor HTTP
+const server = http.createServer((req, res) => {
 
-// Criaçao do servidor HTTP
-const server = http.createServer((req, res)=>{
+    // Captura o método da requisição (GET, POST, PUT ou DELETE)
+    const metodo = req.method;
 
-  // Captura do metodo da requisição HTTP (GET, POST, PUT, DELETE)
-  const metodo = req.method
+    // Captura a URL da requisição (/livros)
+    const url = req.url
 
-// Captura da URL da requisição (/livros)
-  const url = req.url
+    // Define que a resposta será em JSON
+    res.setHeader('Content-Type', 'application/JSON');
+ 
+    // Construindo o método GET - Listar livros
+    if (url === '/livros' && metodo === 'GET') {
 
-  // definindo o tipo de conteúdo da resposta como JSON
-  res.setHeader('content-type', 'application/JSON');
+        res.statusCode = 200; // sucesso
 
-  // Contruindo o metodo GET para a rota /livros
-  if (url === "/livros" && metodo === "GET") {
+        // Retornará a lista de livros em formato JSON
+        res.end(JSON.stringify(livros));
 
-    res.statusCode = 200; // Status de sucesso
-    res.end(JSON.stringify(livros));// Envia a lista de livros como resposta
-    return; // Encerra a execução da função para evitar que o código continue
-  };
+        return; // encerra a execução dessa requisição.
+    };
+ 
+    // Construindo o método POST - Cadastrar livro
+    if (url === '/livros' && metodo === 'POST') {
 
-  // Contruindo o metodo POST - para criar um novo livro
-  if (url === "/livros" && metodo === "POST") {
-    // variavel para armazenar os dados enviados no body da requisição
-    let body = '';
+        // Variável para armazenar os dados enviados no body
+        let body = '';
 
-    // evento disparando quando chegam pertes de dados no body da requisição
-    res.on('data', parte => {
-      body += parte; // Acumula as partes do body
-    });
+        // Evento disparado quando chegam pedaços da requisição
+        req.on('data', parte => {
 
-    // evento a ser disparado depois que todos os dados chegam 
-    req.on('end', () => {
-      const novoLivro = JSON.parse(body);// Converte o JSON recebido em um objeto JavaScript
-      livros.push(novoLivro); // Adiciona o novo livro à lista de livros
+            body += parte; // concatena os pedaços
+        });
 
-      res.statusCode = 201; // criado com sucesso
+        // Evento a ser disparado depois que todos os dados chegam
+        req.on('end', () => {
 
-      res.end(JSON.stringify({
-        mensagem: "Livro criado com sucesso",
-        livro: novoLivro
-      })); // Envia o novo livro criado como resposta
-    });
-    return; // Encerra a execução da função para evitar que o código continue
-  };
+            // Converte o JSON recebido em objeto JavaScript
+            const novoLivro = JSON.parse(body);
 
-  // Contruindo o metodo PUT - para atualizar um livro existente
-  if (url.startsWith("/livros/") && metodo === "PUT") {
-    // variavel para armazenar os dados enviados no body da requisição
-    let body = '';
+            // Adiciona o novo livro ao array de livros
+            livros.push(novoLivro);
 
-    // evento disparando quando chegam pertes de dados no body da requisição
-    res.on('data', parte => {
-      body += parte; // Acumula as partes do body
-    });
+            res.statusCode = 201; // Criado com sucesso
+ 
+            res.end(JSON.stringify({
+                mensagem: 'Livro criado com sucesso',
+                livro: novoLivro
+            }));
+        });
+        return; // encerra a requisição
+    };
+ 
+    // Construindo o método PUT - Atualizar livro
+    if(url === '/livros' && metodo === 'PUT') {
+        // Variável para armazenar os dados enviados no body
+        let body = '';
+        // Evento disparado quando chegam pedaços da requisição
+        req.on('data', parte => {
+            body += parte; // concatena os pedaços
+        });
 
-    req.on('end', () => {
-      // recebe todos os dados atualizados do cliente
-      const livroAtualizado = JSON.parse(body);
-      // percorre a lista de livros (array) e substitui o livro antigo pelo livro atualizado
-      livros = livros.map(livro => {
-        // Verifica se o ID do livro atual é igual ao ID do livro atualizado
-        if (livro.id === livroAtualizado.id) {
-          return livroAtualizado; // Retorna o livro atualizado
-        };
-        return livro; // Retorna o livro original
-      });
-      res.statusCode = 200; // Sucesso
-      res.end(JSON.stringify({
-        mensagem: "Livro atualizado com sucesso",
-        livros: livros
-      }));
-    });
-  };
+        req.on('end', () => {
+            // Recebe todos os dados atualizados vindos do cliente
 
-  // Rota nao encontrada
-  res.statusCode = 404; // Status de recurso nao encontrado
+            const livroAtualizado = JSON.parse(body);
+            // Percorre a lista de livros (array) e substitui o livro com ID igual
+            livros = livros.map(livro => {
+                // Se encontrar o mesmo ID, substitui
+                if(livro.id === livroAtualizado.id) {
+                    return livroAtualizado;
+                };
+                // Devolve todas as atualizações e o que não mudar, retorna igual.
+                return livro;
+            });
+            res.statusCode = 200; // sucesso
+            res.end(JSON.stringify({
+                mensagem: 'Livro atualizado com sucesso',
+                livros: livros
+            }));
+        });
+        return;
+    };
+ 
+    // Construindo o método DELETE - remover livro
+    if(url === "/livros" && metodo === "DELETE") {
+        let body = '';
+ 
+        req.on('data', parte => {
+            body += parte;
+        });
+ 
+        req.on('end', () => {
+            // Recebe o ID do livro a ser removido
+            const dados = JSON.parse(body);
+ 
+            // Filtra o array, removendo o livro com o ID informado.
+            // dessa forma, o array de livros será mantido apenas com os objetos cujo ID é diferente do removido.
+            livros = livros.filter(livro => livro.id !== dados.id);
+ 
+            res.statusCode = 200; // sucesso
+ 
+            res.end(JSON.stringify({
+                mensagem: 'Livro removido com sucesso!',
+                livros: livros
+            }));
+        });
+        return;
+    }
+    // Rota não encontrada
+    res.statusCode = 404; // Não encontrado
 
-  res.end(JSON.stringify({mensagem: "Recurso não encontrado"})); // Envia uma mensagem de erro como resposta
+    // Convertendo a resposta em JSON e exibindo a mensagem
+    res.end(JSON.stringify({
+        mensagem: 'Rota não encontrada'
+    }));
 });
-
 server.listen(3000, () => {
-  console.log(`Servidor rodando em http://localhost:3000/`);
+    console.log('Server running in http://localhost:3000');
 });
+ 
